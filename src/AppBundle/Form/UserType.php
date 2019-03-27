0ef4186ebff4c768e45f8c6d511b54ecad33ef3f
Bastien Vacherand <coco2053@hotmail.com>
2019-03-27 17:52:37 +0100

    Fixes #9; Added CRSF token to forms

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -9,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
@@ -32,5 +34,13 @@ class UserType extends AbstractType
                 'multiple' => true,
             ))
         ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class'      => User::class,
+            'csrf_protection' => true,
+        ]);
     }
 }
